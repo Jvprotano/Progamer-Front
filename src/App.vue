511205d27cf1b6572d6b9323ae5856fa82e6105f
Gustavo Kenji <gustavosumi11@gmail.com>
2021-08-19 19:15:08 -0300

    Arrumando header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <Header/>
+    <MenuLateral/>
     <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import MenuLateral from './components/MenuLateral'
 
 
 export default {
   name: 'App',
   components:{
     Header,
+    MenuLateral,
   }
 }
 </script>
@@ -28,49 +31,6 @@ input{
   text-align: center;
 }
 
-#formulario {
-  background-color: #fff;
-  margin: 0;
-  text-align: center;
-}
-#menu-h{
-  background-color:#533784;
-}
-#menu-h ul {
-    list-style: none;
-    padding: 0;
-    margin-block: 0;
-}
-#menu-h ul li {
-    display: inline;
 
-}
-#menu-h ul li a {
-    color: #FFF;
-    padding: 25px;
-    display: inline-block;
-    text-decoration: none;
-    transition: background .5s;
-}
-#menu-h ul li a:hover {
-    background-color: #0F0C1D;
-
-}
-#menu-h ul li:last-child a {
-    float: right;
-    background-color: #6a5ca1;
-}
-#pesquisa{
-    padding: 0 15px;
-    border: 10px;
-    border-radius: 3px;
-    margin-left: 10px;
-    margin-right: 10px;
-    font-size: 16px;
-
-}
-#paginaHome{
-    float: left;
-}
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,15 @@
     <div id="menu">
  <nav id="menu-h">
         <ul>
-            <li><a href="#" id="paginaHome">Home</a></li>
+            <li><router-link id="paginaHome" to="/">Home</router-link></li>
 
-            <li><input type="text" id="pesquisa" placeholder="Pesquisar"></li>
+            <li><input type="text" id="pesquisa" placeholder="Faça sua pesquisa"><button>Pesquisar</button></li>
+            
+            <li><router-link to="/">Sobre</router-link></li>
 
-            <li><a href="#">Sobre</a></li>
+            <li><router-link to="/cadastrar">Cadastre-se</router-link></li>
 
-            <li><a href="#">Quem Somos</a></li>
-
-            <li><a href="#">Contato</a></li>
+            <li><router-link to="/">Quem somos</router-link></li>
 
             <li><a href="#" id="botLogin">Entrar</a></li>
         </ul>
@@ -24,6 +24,49 @@ export default {
 }
 </script>
 
-<style >
+<style scoped>
+#formulario {
+  background-color: #fff;
+  margin: 0;
+  text-align: center;
+}
+#menu-h{
+  background-color:#533784;
+}
+#menu-h ul {
+    list-style: none;
+    padding: 0;
+    margin-block: 0;
+}
+#menu-h ul li {
+    display: inline;
 
+}
+#menu-h ul li a {
+    color: #FFF;
+    padding: 25px;
+    display: inline-block;
+    text-decoration: none;
+    transition: background .5s;
+}
+#menu-h ul li a:hover {
+    background-color: #0F0C1D;
+
+}
+#menu-h ul li:last-child a {
+    float: right;
+    background-color: #6a5ca1;
+}
+#pesquisa{
+    padding: 0 15px;
+    border: 10px;
+    border-radius: 3px;
+    margin-left: 10px;
+    margin-right: 10px;
+    font-size: 16px;
+
+}
+#paginaHome{
+    float: left;
+}
 </style>
