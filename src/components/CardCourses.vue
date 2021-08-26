70b7780f4adb2633eb29dc01079731bd597b9955
Gustavo Kenji <gustavosumi11@gmail.com>
2021-08-25 22:44:18 -0300

    Criação da página home com listagens.

---
+++ src/components/CardCourses.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="container">
+       <b-row>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/M0ll8r3w3WsiRLLroy5xJaK4A5c=/0x0:1656x915/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/N/j/3cZcm6Qf6TxFIAW0nMNg/como-baixar-valorant-de-graca-the-squad.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+       <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/M0ll8r3w3WsiRLLroy5xJaK4A5c=/0x0:1656x915/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/N/j/3cZcm6Qf6TxFIAW0nMNg/como-baixar-valorant-de-graca-the-squad.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+      
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://midias.jb.com.br/_midias/jpg/2021/03/11/csgo-586394.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/ps1kBYBuji6g04QRr8OKLdOoSNQ=/0x0:695x434/924x0/smart/filters:strip_icc()/s.glbimg.com/po/tt2/f/original/2016/11/25/gta-v-leak-image-1.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+    </b-row>
+<hr>
+<h1>Cursos</h1>
+<hr>
+       <b-row>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/M0ll8r3w3WsiRLLroy5xJaK4A5c=/0x0:1656x915/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/N/j/3cZcm6Qf6TxFIAW0nMNg/como-baixar-valorant-de-graca-the-squad.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+       <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/M0ll8r3w3WsiRLLroy5xJaK4A5c=/0x0:1656x915/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2020/N/j/3cZcm6Qf6TxFIAW0nMNg/como-baixar-valorant-de-graca-the-squad.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+      
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://midias.jb.com.br/_midias/jpg/2021/03/11/csgo-586394.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/ps1kBYBuji6g04QRr8OKLdOoSNQ=/0x0:695x434/924x0/smart/filters:strip_icc()/s.glbimg.com/po/tt2/f/original/2016/11/25/gta-v-leak-image-1.jpg"
+            img-alt="Image"
+            img-top
+          >
+          </b-card>
+        </b-card-group>
+      </b-col>
+    </b-row>
+  
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-deck {
+  width: 200px;
+  height: 100px;
+  margin-bottom: 40%;
+  margin-left: 24%;
+  margin-top: 16%;
+}
+</style>
---
+++ src/components/CoursesList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <vue-glide>
+      <vue-glide-slide v-for="i in 10" :key="i">
+        <img id="imgGame" src="https://s2.glbimg.com/btDsdKAb7R9NsomF5cvsF8IIBwg=/0x0:795x597/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/r/D/0my8teTCWaRrTLpQfNZg/7.jpg"/>
+        Slide {{ i }}
+        
+      </vue-glide-slide>
+    </vue-glide>
+  </div>
+</template>
+ 
+<script>
+  import { Glide, GlideSlide } from 'vue-glide-js'
+ 
+  export default {
+    components: {
+      [Glide.name]: Glide,
+      [GlideSlide.name]: GlideSlide
+    }
+  }
+</script> 
+<style scoped>
+
+</style>
