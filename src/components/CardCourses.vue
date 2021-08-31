e8cc43fbac64bae909a2c7f573497ace9c7a922a
Gustavo Kenji <gustavosumi11@gmail.com>
2021-08-30 21:51:51 -0300

    Alterando CSS e cards

--- src/components/CardCourses.vue
+++ src/components/CardCourses.vue
@@ -1,139 +1,161 @@
 <template>
   <b-container class="bv-example-row">
+    <hr />
+    <h1>Jogos em destaque</h1>
+    <hr />
     <b-row>
       <b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://i.ytimg.com/vi/O8KkJvpz2xc/maxresdefault.jpg" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-<b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://s2.glbimg.com/USoUR0oDveQ_AVGUQtwoa1gdx68=/0x0:1280x960/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/s/e/BU7M0zTY2bKdjLAKBtkw/counter-strike-global-offensive-screenshot-2021.02.08-14.54.58.88.png" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-<b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://theclutch.com.br/wp-content/uploads/2021/03/csgo-capa-cts.jpg" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-<b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://mundoconectado.com.br/uploads/chamadas/cs_go_chamada.jpg" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-</b-row>
- <b-row>
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://i.ytimg.com/vi/O8KkJvpz2xc/maxresdefault.jpg"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
       <b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://i.ytimg.com/vi/O8KkJvpz2xc/maxresdefault.jpg" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-<b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://s2.glbimg.com/USoUR0oDveQ_AVGUQtwoa1gdx68=/0x0:1280x960/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/s/e/BU7M0zTY2bKdjLAKBtkw/counter-strike-global-offensive-screenshot-2021.02.08-14.54.58.88.png" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-<b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://theclutch.com.br/wp-content/uploads/2021/03/csgo-capa-cts.jpg" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-<b-col sm="3">
-  <b-card-group deck>
-    <b-card title="Title" img-src="https://mundoconectado.com.br/uploads/chamadas/cs_go_chamada.jpg" img-alt="Image" img-top>
-      <b-card-text>
-        This is a wider card with supporting text below as a natural lead-in to additional content.
-        This content is a little bit longer.
-      </b-card-text>
-      <template #footer>
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </template>
-    </b-card>
-
-  </b-card-group>
-</b-col>
-</b-row>
-
-
-</b-container>
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/USoUR0oDveQ_AVGUQtwoa1gdx68=/0x0:1280x960/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/s/e/BU7M0zTY2bKdjLAKBtkw/counter-strike-global-offensive-screenshot-2021.02.08-14.54.58.88.png"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://theclutch.com.br/wp-content/uploads/2021/03/csgo-capa-cts.jpg"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://mundoconectado.com.br/uploads/chamadas/cs_go_chamada.jpg"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+    </b-row>
+    <hr />
+    <h1>Cursos em destaque</h1>
+    <hr />
+    <b-row>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://i.ytimg.com/vi/O8KkJvpz2xc/maxresdefault.jpg"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://s2.glbimg.com/USoUR0oDveQ_AVGUQtwoa1gdx68=/0x0:1280x960/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/s/e/BU7M0zTY2bKdjLAKBtkw/counter-strike-global-offensive-screenshot-2021.02.08-14.54.58.88.png"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://theclutch.com.br/wp-content/uploads/2021/03/csgo-capa-cts.jpg"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+      <b-col sm="3">
+        <b-card-group deck>
+          <b-card
+            title="Title"
+            img-src="https://mundoconectado.com.br/uploads/chamadas/cs_go_chamada.jpg"
+            img-alt="Image"
+            img-top
+          >
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
 img {
+  box-shadow: 0 0 40px rgb(0 0 0 / 15%);
   height: 300px;
-  width: 300px;
+  object-fit: cover;
+  border-radius: 10px !important;
+  box-sizing: border-box;
 }
-.row{
+.row {
   margin-top: 10px;
   margin-bottom: 10px;
+  object-fit: cover;
 }
 </style>
--- src/components/CoursesList.vue
+++ src/components/CoursesList.vue
@@ -1,15 +1,27 @@
 <template>
+<div class="carousel">
+  <div>
+    <h1>DEEPWEB SITE</h1>
+  </div>
   <splide :options="options">
+    
     <splide-slide>
+      <b-card>
       <img src="https://img.redbull.com/images/c_crop,x_288,y_0,h_675,w_787/c_fill,w_650,h_540/q_auto,f_auto/redbullcom/2021/7/13/qcmg7aaz9yn153jogvko/games-counter-strike-global-offensive">
+      </b-card>
     </splide-slide>
     <splide-slide>
+      <b-card>
       <img src="https://s2.glbimg.com/pNs-qBykHRMapHzDUV1PEoi3NKM=/0x0:695x391/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2019/9/Y/vKorkqTcOZfmutlwGE7Q/1.jpg">
+      </b-card>
     </splide-slide>
     <splide-slide>
+      <b-card>
       <img src="https://i.ytimg.com/vi/icBEmeEJV3M/maxresdefault.jpg">
+      </b-card>
     </splide-slide>
   </splide>
+  </div>
 </template>
 
 <script>
@@ -24,10 +36,19 @@ import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
       return {
         options: {
           rewind : true,
-          width  : 800,
-          gap    : '1rem',
+          width  : 2000,
+          gap    : 'rem',
         },
       };
     },
   }
 </script>
+<style scoped>
+img {
+  width: 100% !important;
+  height: 500px !important;
+   object-fit: cover;
+    box-shadow: 0 0 40px rgb(0 0 0 / 15%);
+    
+}
+</style>
