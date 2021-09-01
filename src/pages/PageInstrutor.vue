4c1c39aec157af51645d541a02eb10d0534878dc
Gustavo Boroski <gugamalheiro@gmail.com>
2021-08-31 22:29:06 -0300

    Desenvolvimento da tela page instrutor 80% - concluido

---
+++ src/pages/PageInstrutor.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="home-i">
+    <b-container>
+        <ul>
+            <li><b-button id="cursoInstrutor" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Cursos Instruidos</b-button></li>
+            <li><b-button id="historicoFinanceiro" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Histórico Financeiro</b-button></li>
+            <li><b-button id="minhasVendas" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Minhas Vendas</b-button></li>
+        </ul>
+    </b-container>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#home-i{
+    width: 1325px; 
+    height: 80px; 
+    background-color: rgb(122, 117, 117); 
+    margin-top: -16px;
+    padding: 20px;
+}
+#home-i ul li {
+    margin-left: 10px;
+    margin-right: 10px;
+    display: inline;
+}
+.cursoInstrutor{
+    width: 500px; 
+    height: 80px; 
+    background-color: rgb(0, 0, 0); 
+    margin-top: -6px;
+    padding: 20px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './pages/Home';
-
+import PageInstrutor from './pages/PageInstrutor';
 
 Vue.use(Router);
 
@@ -12,7 +12,11 @@ const router = new Router({
      {
          path: '/',
          component: Home
-     }
+     },
+     {
+        path: '/pageInstrutor',
+        component: PageInstrutor
+    }
  ]
 });
 
