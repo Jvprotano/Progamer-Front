78c639b0a82189b219a31e13b8ad38d2cacd1a4f
Gustavo Boroski <gugamalheiro@gmail.com>
2021-09-01 21:43:32 -0300

    Melhorando HTML e CSS dos botões e inicio da tela de solicitação

--- src/pages/PageInstrutor.vue
+++ src/pages/PageInstrutor.vue
@@ -1,12 +1,10 @@
 <template>
 <div id="home-i">
-    <b-container>
-        <ul>
-            <li><b-button id="cursoInstrutor" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Cursos Instruidos</b-button></li>
-            <li><b-button id="historicoFinanceiro" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Histórico Financeiro</b-button></li>
-            <li><b-button id="minhasVendas" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Minhas Vendas</b-button></li>
-        </ul>
-    </b-container>
+    <ul>
+        <li><a href="#"><b-button id="cursoInstrutor" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Cursos Instruidos</b-button></a></li>
+        <li><a href="#"><b-button id="historicoFinanceiro" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Histórico Financeiro</b-button></a></li>
+        <li><a href="#"><b-button id="minhasVendas" variant="light" size="lg" style="margin-top: -10px; border: 5px solid rgb(0,0,0)">Minhas Vendas</b-button></a></li>
+    </ul>
 </div>
 </template>
 
@@ -27,11 +25,7 @@ export default {}
     margin-right: 10px;
     display: inline;
 }
-.cursoInstrutor{
-    width: 500px; 
-    height: 80px; 
-    background-color: rgb(0, 0, 0); 
-    margin-top: -6px;
-    padding: 20px;
+#home-i ul li a:hover {
+    background-color: #0F0C1D !important;
 }
 </style>
